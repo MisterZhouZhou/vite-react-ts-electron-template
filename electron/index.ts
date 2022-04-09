9357f7e6c229211a7f195b4a1a35028d0acd66d8
zhouwei <zhouwei1@cheyipai.com>
2022-04-09 11:50:49 +0800

    更新模版

--- electron/index.ts
+++ electron/index.ts
@@ -1,59 +1,59 @@
-import { app, BrowserWindow } from "electron";
-import { resolve } from "path";
+import { app, BrowserWindow } from 'electron'
+import { resolve } from 'path'
 
-const height = 800;
-const width = 1200;
+const height = 800
+const width = 1200
 
 // 是否为开发环境
-const isDev = process.env.IS_DEV == "true" ? true : false;
-const pathResolve = (dir: string) => resolve(__dirname, dir);
+const isDev = process.env.IS_DEV == 'true' ? true : false
+const pathResolve = (dir: string) => resolve(__dirname, dir)
 
-let win: BrowserWindow;
+let win: BrowserWindow
 const createWindow = () => {
-  // 创建win
-  win = new BrowserWindow({
-    width: width,
-    height: height,
-    frame: true,
-    show: true,
-    resizable: true,
-    fullscreenable: true,
-    webPreferences: {
-      preload: pathResolve("preload.js"),
-    },
-  });
+	// 创建win
+	win = new BrowserWindow({
+		width: width,
+		height: height,
+		frame: true,
+		show: true,
+		resizable: true,
+		fullscreenable: true,
+		webPreferences: {
+			preload: pathResolve('preload.js')
+		}
+	})
 
-  // const port = process.env.PORT || 3000;
-  if (isDev) {
-    // 加载本地服务
-    win.loadURL(`http://localhost:3000`);
-    // 打开开发者工具
-    win.webContents.openDevTools();
-    return;
-  }
-  // 加载生成环境路径
-  win.loadFile(pathResolve("../dist/index.html"));
-};
+	// const port = process.env.PORT || 3000;
+	if (isDev) {
+		// 加载本地服务
+		win.loadURL(`http://localhost:3000`)
+		// 打开开发者工具
+		win.webContents.openDevTools()
+		return
+	}
+	// 加载生成环境路径
+	win.loadFile(pathResolve('../index.html'))
+}
 
 app.whenReady().then(() => {
-  // 创建window
-  createWindow();
-  // 创建服务
-  // createServer()
-  // 监听app激活
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
+	// 创建window
+	createWindow()
+	// 创建服务
+	// createServer()
+	// 监听app激活
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow()
+		}
+	})
+})
 
 // 监听所有窗口关闭
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+})
 
 // ipcMain.on('message', (event: IpcMainEvent, message: any) => {
 //   console.log(message);
